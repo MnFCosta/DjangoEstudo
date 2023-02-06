a083bca8b04bb05db507106f15a9500fa3ab2b4a
Manoel Costa <mnfcosta1@gmail.com>
2023-02-06 10:23:25 -0300

    módulo Models e ORM completo, adicionado template de convite com numeração automática

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-    
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,7 @@ from recipes import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.view, name="home"),
+    path('', views.teste, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
 ]
